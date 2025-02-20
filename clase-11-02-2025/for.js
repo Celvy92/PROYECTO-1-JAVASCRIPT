307e366d7b2a6d834ec46b426647806faaef5a55
Celeste <celeste.ipn92@gmail.com>
2025-02-19 23:14:52 -0600

    Se terminaron los ejemplos de como utilizar for

---
+++ clase-11-02-2025/for.js
@@ -0,0 +1,17 @@
+//Ejemplo 1: contador simple con for
+
+for (let contador = 1; contador <= 5; contador++) {
+console.log("(FOR) El contador está en el ciclo:"; Contador);
+}
+
+//Ejemplo 2: Contador en reversa con for
+for (let cuentaRegresiva = 10; cuentaRegresiva > 0; cuentaRegresiva--) {
+    console.log("(FOR) Cuenta regresiva", cuentaRegresiva);
+} 
+
+//Ejemplo 3: Saltarnos los números pares
+for(let) impar = 1; impar <=10; impar +=2){
+    console.log("Números impar: ", impar);
+
+}
+
