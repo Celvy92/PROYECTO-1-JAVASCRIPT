3534cc14f90341d962cc97069fea6d5a13b96deb
Celeste <celeste.ipn92@gmail.com>
2025-02-27 12:26:06 -0600

    Cree el archivo seguimiento de libros para la práctica 4

--- clase-13-02-2025/funciones.js
+++ clase-13-02-2025/funciones.js
@@ -12,5 +12,50 @@ function saludar (){
 }
 
 
-//Paso 2. Llamar a la función 
+//Paso 2. Llamar a la función, o la estamos ejecutando
 saludar()
+saludar()
+saludar()//Puedo llamarla cuantas veces quiera 
+
+
+//Creo una función parámetros y argumentos
+//Los parámetros son variables que una función recibe al ser llamada
+function saludarPersona(nombre) {
+    console.log("Hola ", nombre, "!");
+}
+
+//Aquí "nombre" es un parámetro y "Blanca" es el argumento
+saludarPersona("Blanca")
+saludarPersona("Alejandra")
+saludarPersona("Cipriano")
+
+//Si no especificamos un return, la función nos devuelve undefined
+console.log(saludar());
+console.log(saludarPersona("Kitty"));
+
+ //Una función puede devolver un valor con "return"
+ function suma(num1, num2){
+    return num1 + num2
+ }
+
+ console.log(suma(9,6));
+ console.log(suma(7,10));
+ console.log(suma("Hola"," a todos"));
+
+ function resta(num1, num2){
+    let resta = num1 - num2
+    return resta
+ }
+
+ console.log(resta(2,10))//Guardamos el resultado en una variable
+ console.log(resultado);
+
+ function saludoMayuscula(nombre) {
+    function mayuscula(string){
+    return string.toUpperCase()
+ }
+ return mayuscula (nombre)
+}
+
+console.log(saludoMayuscula)("Kendra"));
+//console.log(mayuscula("Hola")); //Devuelve error porque mayuscula sólo vive dentro de saludoMayuscula
