621c58686d5cb6d343e411c4bf71f8c9beef3929
Celeste <celeste.ipn92@gmail.com>
2025-02-27 00:04:13 -0600

    Se agregaron ejemplos funciones

---
+++ clase-13-02-2025/funciones.js
@@ -0,0 +1,16 @@
+//FUNCIONES CON JAVASCRIPT
+
+/*
+    Una función es un bloque de código REUTILIZABLE que realiza una tarea en especifico
+    Nos permite escribir código más organizado y evitar repetir instrucciones 
+*/
+
+
+//Sintaxis básica de una función
+function saludar (){
+    console.log("Hola mundo!");
+}
+
+
+//Paso 2. Llamar a la función 
+saludar()
